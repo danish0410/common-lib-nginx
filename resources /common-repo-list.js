40805a68f790aec4e299787a12a692f7ef03877e
danish0410 <thanigai_velan@yahoo.com>
2025-07-14 12:12:33 +0530

    Create common-repo-list.js

---
+++ resources /common-repo-list.js
@@ -0,0 +1,11 @@
+{
+  "nginx": [
+    {
+      "repo-name": "nginx-p81",
+      "project_dir": ".",
+      "git-url": "git@github.com:danish0410/nginx-p81.git",
+      "dockerhub_username": "thanigai2808",
+      "git_credentials_id": "private-key-jenkins"
+    }
+  ]
+}
