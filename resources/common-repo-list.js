b3df9c549f9fc27f26841ad3b9ac5a9ec55e41d4
danish0410 <thanigai_velan@yahoo.com>
2025-07-14 18:19:11 +0530

    Update common-repo-list.js

--- resources/common-repo-list.js
+++ resources/common-repo-list.js
@@ -6,6 +6,13 @@
       "git-url": "git@github.com:danish0410/nginx-p81.git",
       "dockerhub_username": "thanigai2808",
       "git_credentials_id": "private-key-jenkins"
+    },
+    {
+      "repo-name": "php-nginx-helloworld",
+      "project_dir": ".",
+      "git-url": "git@github.com:danish0410/php-nginx-helloworld.git",
+      "dockerhub_username": "thanigai2808",
+      "git_credentials_id": "private-key-jenkins"
     }
   ]
 }
